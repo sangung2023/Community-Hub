c6b48d4e4362987eb900d481dd841d14c21738b0
tkddnd010 <tkddnd010@naver.com>
2025-08-23 22:17:04 +0900

    middleware 에러 해결

--- src/app.js
+++ src/app.js
@@ -16,5 +16,5 @@ app.use('/api', [UsersRouter, PostsRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(PORT, () => {
-  console.log(PORT, '포트로 서버가 열렸어요!');
+	console.log(PORT, '포트로 서버가 열렸어요!');
 });
--- src/middlewares/log.middleware.js
+++ src/middlewares/log.middleware.js
@@ -1,19 +1,19 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+	level: 'info',
+	format: winston.format.json(),
+	transports: [new winston.transports.Console()],
 });
 
 export default function (req, res, next) {
-  const start = new Date().getTime();
+	const start = new Date().getTime();
 
-  res.on('finish', () => {
-    const duration = new Date.getTime() - start;
-    logger.info(
-      `Method: ${req.method}, URL: ${req.url}, Status: ${req.statusCode}, Duration: ${duration}ms`
-    );
-  });
-  next();
+	res.on('finish', () => {
+		const duration = new Date().getTime() - start;
+		logger.info(
+			`Method: ${req.method}, URL: ${req.url}, Status: ${req.statusCode}, Duration: ${duration}ms`
+		);
+	});
+	next();
 }
