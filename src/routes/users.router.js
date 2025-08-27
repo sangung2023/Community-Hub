6514c8282dfb0b609d4b75d8750026e681e73163
unknown <tkddnd1211@gmail.com>
2025-08-27 09:45:42 +0900

    회원가입 transaction

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -3,6 +3,7 @@ import { prisma } from '../utils/prisma/index.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import { Prisma } from '@prisma/client';
 
 const router = express.Router();
 
@@ -21,19 +22,28 @@ router.post('/sign-up', async (req, res, next) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await prisma.users.create({
-      data: { email, password: hashedPassword },
-    });
+    const [user, userInfo] = await prisma.$transaction(
+      async (tx) => {
+        const user = await tx.users.create({
+          data: { email, password: hashedPassword },
+        });
 
-    const userInfo = await prisma.userInfos.create({
-      data: {
-        userId: user.userId,
-        name,
-        age,
-        gender,
-        profileImage,
+        const userInfo = await tx.userInfos.create({
+          data: {
+            userId: user.userId,
+            name,
+            age,
+            gender,
+            profileImage,
+          },
+        });
+
+        return [user, userInfo];
       },
-    });
+      {
+        isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+      }
+    );
 
     return res.status(201).json({ message: '회원가입에 성공하였습니다.' });
   } catch (err) {
